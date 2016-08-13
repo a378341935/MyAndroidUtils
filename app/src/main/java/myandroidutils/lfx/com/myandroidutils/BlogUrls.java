fe2fc34262e30f8a790b89b72b9191d2e0e089ba
a378341935 <vigneliufx@163.com>
2016-08-13 10:41:37 +0800

    URLs Update

---
+++ app/src/main/java/myandroidutils/lfx/com/myandroidutils/BlogUrls.java
@@ -0,0 +1,19 @@
+package myandroidutils.lfx.com.myandroidutils;
+
+/*Include Useful info about Android Development*/
+public class BlogUrls {
+    /**
+     * 屏幕适配
+     */
+    public static final String FIT_SCREEN = "http://www.cocoachina.com/android/20151030/13971.html";
+
+    /**
+     * Fresco第三方图片处理
+     */
+    public static final String FRESCO = "http://www.fresco-cn.org/";
+
+    /**
+     * Android性能优化
+     */
+    public static final String ANDROID_OPTIMIZATION = "http://blog.csdn.net/woyaowenzi/article/details/9273839";
+}
